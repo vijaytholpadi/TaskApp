1f9874ae0d0559450d1adf73930c572703f3661e
Vijay Tholpadi <vijay@thegeekprojekt.com>
2015-02-28 14:22:57 +0530

    Storyboard Layout for Detail view and Settings view.

---
+++ AppointDemo/AppointDemo/Controllers/ADAppointmentDetailViewController/ADAppointmentDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADAppointmentDetailViewController.h
+//  AppointDemo
+//
+//  Created by Vijay Tholpadi on 2/28/15.
+//  Copyright (c) 2015 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAppointmentDetailViewController : UIViewController
+
+@end
